792460a883789c05009ea60137a488d30588e432
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 13:03:23 -0700

    Added collections.Counter() to count word occurrences.

--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,6 @@
 # put your code here.
 
-import sys
+import sys, collections
 
 #Open the  file:
 
@@ -17,19 +17,23 @@ for line in text_file:
     #Add words to master list
     all_words_in_file.extend(line)
 
-word_counts = {}
+word_counts = collections.Counter(all_words_in_file)
+print word_counts
+
+
+# word_counts = {}
 
 #Loop over master word list
-for word in all_words_in_file:
+# for word in all_words_in_file:
 
-    clean_word = word.strip('.!@#$%^&*()-?')
-    clean_word = clean_word.lower()
+#     clean_word = word.strip('.!@#$%^&*()-?')
+#     clean_word = clean_word.lower()
 
-    #Add new words to dictionary. Otherwise increment count.
-    word_counts[word] = word_counts.get(word, 0) + 1
+#     #Add new words to dictionary. Otherwise increment count.
+#     word_counts[word] = word_counts.get(word, 0) + 1
 
-for item in word_counts.iteritems():
-    print item
+# for item in word_counts.iteritems():
+#     print item
 
 # for tup in word_counts.items():
 #     print "%s: %d" % (tup[0], tup[1])
