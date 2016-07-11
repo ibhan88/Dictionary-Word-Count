e4a7d32d833095c3a314b1e19c75b9fb71aa35a4
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 12:16:43 -0700

    Implement Word Counting.

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,24 @@
 # put your code here.
+
+#Open the  file:
+
+text_file = open("test.txt")
+
+all_words_in_file = []
+
+for line in text_file:
+
+    #Create list of words in line
+    line = line.rstrip()
+    line = line.split(" ")
+    all_words_in_file.extend(line)
+
+word_counts = {}
+
+for word in all_words_in_file:
+    word_counts[word] = word_counts.get(word, 0) + 1
+
+print word_counts
+
+
+
