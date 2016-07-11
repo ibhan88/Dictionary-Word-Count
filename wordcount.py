741a4fac11c215fe2043905e60395b35c20c0cd9
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 12:18:03 -0700

    Add explanatory comments.

--- wordcount.py
+++ wordcount.py
@@ -11,11 +11,16 @@ for line in text_file:
     #Create list of words in line
     line = line.rstrip()
     line = line.split(" ")
+
+    #Add words to master list
     all_words_in_file.extend(line)
 
 word_counts = {}
 
+#Loop over master word list
 for word in all_words_in_file:
+
+    #Add new words to dictionary. Otherwise increment count.
     word_counts[word] = word_counts.get(word, 0) + 1
 
 print word_counts
